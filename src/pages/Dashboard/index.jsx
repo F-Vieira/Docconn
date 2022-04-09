4a56c82e71d1f0c6d86f9f3c0190667e095511c2
Fernando Vieira <fernando.santos2901@gmail.com>
2022-04-09 11:00:31 -0300

    feat: simple structure and style of daily calls

--- src/pages/Dashboard/index.jsx
+++ src/pages/Dashboard/index.jsx
@@ -9,6 +9,15 @@ const Dashboard = () => {
         <Indicator title="Retornos" amount="12" />
         <Indicator title="Pacientes atendidos hoje" amount="12" />
       </D.Indicators>
+      <D.Calls>
+        <h2>Atendimentos de Hoje</h2>
+        <ul>
+          <li>paciente 1</li>
+          <li>paciente 2</li>
+          <li>paciente 3</li>
+          <li>paciente 4</li>
+        </ul>
+      </D.Calls>
     </D.Container>
   );
 };
--- src/pages/Dashboard/styles.jsx
+++ src/pages/Dashboard/styles.jsx
@@ -1,7 +1,34 @@
 import styled from "styled-components";
 
-export const Container = styled.main``;
+export const Container = styled.main`
+  height: 100vh;
+  padding: 5% 20%;
+  display: flex;
+  flex-direction: column;
+`;
 
 export const Indicators = styled.section`
   display: flex;
+  justify-content: space-around;
+`;
+
+export const Calls = styled.section`
+  h2 {
+    font-size: 2rem;
+    margin: 15px 0;
+    margin-left: 20px;
+  }
+
+  ul {
+    li {
+      width: 100%;
+      height: 30px;
+      line-height: 30px;
+      padding: 0 15px;
+
+      background-color: var(--white);
+      border-radius: 10px;
+      margin-bottom: 10px;
+    }
+  }
 `;
